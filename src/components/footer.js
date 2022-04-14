3e59c385d4c753256bca131589a24adbe2660f76
OtacilioN <contato@otaciliomaia.com>
2022-04-13 23:19:27 -0300

    update footer info

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,9 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
+      Feito com <a href="https://contentful.com/">Contentful</a> e{' '}
       <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      <a href="https://github.com/OtacilioN/blog">Código Fonte</a>
     </div>
   </Container>
 )
