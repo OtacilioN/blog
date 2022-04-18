784e6a032c69a528ea5f5a6cc008578013e7d9ce
OtacilioN <contato@otaciliomaia.com>
2022-04-18 10:24:54 -0300

    Add UA code instead of g4 code

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ const contentfulConfig = {
 }
 
 const gTagOptions = {
-  trackingIds: ['G-PNN1KK1EG1'],
+  trackingIds: ['UA-150871174-1'],
   gtagConfig: {
     anonymize_ip: true,
     cookie_expires: 0,
